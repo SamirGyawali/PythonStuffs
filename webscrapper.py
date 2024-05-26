380276c5e77a201e9f209eb6b90b80e79707395b
SamirGyawali <gyawali.samir000@gmail.com>
2024-05-26 10:57:44 +0545

    Simple Web Scrapper

---
+++ webscrapper.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+page_to_scrap = requests.get("https://quotes.toscrape.com/")
+soup = BeautifulSoup(page_to_scrap.text, "html.parser")
+quotes = soup.findAll("span", attrs={"class":"text"})
+authors = soup.findAll("small", attrs={"class":"author"})
+
+
+for quote in quotes:
+    print(quote.text)
+for author in authors:
+    print(author.text)
